75f436b3eb83ac6ae7b48e12e165d2ab156165fd
Abdullah Nadeem <Abdullah8650748@gmail.com>
2025-02-04 17:23:55 -0800

    Abdullah First

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
+  console.log(process.env.MONGO_URI); // This will print the Mongo URI to the console
 
 // Routes
 app.use("/api/articles", articleRoutes);
